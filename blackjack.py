63f4bd0ba28b1a546236e186a891357e8f888ca9
DylanHyllan <dylan.aziz@elev.ga.ntig.se>
2024-04-18 12:54:39 +0200

    first commit

---
+++ blackjack.py
@@ -0,0 +1,18 @@
+import random
+system = 0
+player = 0
+stand = False
+
+def card():
+    group = None
+    amount = None
+    group = random.randint(1,4)
+    amount = random.randint(1,13)
+    
+
+while stand = False:
+    ans = input("type 1 to hit or 0 to stand")
+    if ans == 1:
+        
+    elif ans == 0:
+        stand = True
